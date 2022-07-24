88b6ebff890101ac9335d6c7a3eedd6647b5b08e
dodouuu <joe30566@gmail.com>
2022-07-24 11:35:58 +0800

    217. Contains Duplicate

--- app.js
+++ app.js
@@ -1,24 +1,20 @@
 /**
  * @param {number[]} nums
- * @return {number}
+ * @return {boolean}
  */
-var singleNumber = function (nums) {
+var containsDuplicate = function (nums) {
   const l = nums.length
   let a = {}
   for (let i = 0; i < l; i++) {
     if (a[nums[i]] === undefined) {
-      a[nums[i]]=1
-    } else if (a[nums[i]] > 0) {
-      a[nums[i]]++
+      a[nums[i]] = 1
+    } else if (a[nums[i]] === 1) {
+      return true
     }
   }
-  for (const [key, value] of Object.entries(a)) {
-    if(value === 1) {
-      return Number(key)
-    }
-  }
+  return false
 };
 
-const nums = [-1, -1, -2]
+const nums = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2]
 
-singleNumber(nums)
+containsDuplicate(nums)
