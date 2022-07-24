ab50dc5605886fa0763d4ec27c2bc7a4c0777701
dodouuu <joe30566@gmail.com>
2022-07-24 11:27:01 +0800

    136. Single Number

--- app.js
+++ app.js
@@ -1,30 +1,24 @@
 /**
  * @param {number[]} nums
- * @return {void} Do not return anything, modify nums in-place instead.
+ * @return {number}
  */
-var moveZeroes = function (nums) {
-  let temp = -1
+var singleNumber = function (nums) {
   const l = nums.length
-  // let numOfZero = 0
-  // for (let i = 0; i < l; i++) {
-  //   if (nums[i] === 0) numOfZero++
-  // }
-  // if(numOfZero===0) return
-
+  let a = {}
   for (let i = 0; i < l; i++) {
-    if (nums[i] === 0) {
-      for (let j = i + 1; j < l; j++) {
-        if (nums[j] !== 0) {
-          temp = nums[j]
-          nums[j] = nums[i]
-          nums[i] = temp
-          break
-        }
-      }
+    if (a[nums[i]] === undefined) {
+      a[nums[i]]=1
+    } else if (a[nums[i]] > 0) {
+      a[nums[i]]++
+    }
+  }
+  for (const [key, value] of Object.entries(a)) {
+    if(value === 1) {
+      return Number(key)
     }
   }
 };
 
-const nums = [0, 1, 0, 3, 12]
-moveZeroes(nums)
-// console.log('nums=', nums)
+const nums = [-1, -1, -2]
+
+singleNumber(nums)
