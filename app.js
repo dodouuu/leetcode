c95d00451d04a39645f8479aab4fd3f34a74847b
dodouuu <joe30566@gmail.com>
2022-08-12 09:23:04 +0800

    55. Jump Game

--- app.js
+++ app.js
@@ -1,31 +1,25 @@
 /**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
+ * @param {number[]} nums
+ * @return {boolean}
  */
-/**
- * @param {TreeNode} root
- * @return {number[]}
- */
-let stack = []
-let ans = []
-var inorderTraversal = function (root) {
-  stack = []
-  ans = []
-  if(!root) return ans
-  let node = root
-  while(node || stack.length) {
-    if(node) {
-      stack.push(node)
-      node = node.left
-    } else {
-      const top = stack.pop()
-      ans.push(top.val)
-      node = top.right
-    }
-  }
-  return ans
+var canJump = function (nums) {
+  const l = nums.length
+  let dp = Array(l).fill(-1)
+  return recursive(0, nums, dp)
 };
+
+// dp[i] = false => jump to i will be impossible to reach the last index
+ 
+function recursive(i, nums, dp) {
+  if(i >= nums.length-1 ) return true
+  if(dp[i] !== -1) return dp[i]
+
+  for (let j = nums[i]; j>=1; j--) {
+    if (recursive(i+j, nums, dp)) return true
+  }
+  return dp[i] = false
+}
+
+const nums = [3, 2, 1, 0, 4]
+
+console.log(canJump(nums))
